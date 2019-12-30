929e54cc8025ec11c88aa73628ace53b38347d2e
suraj7852 <surajpj7852@gmail.com>
2019-12-30 17:30:32 +0530

    Added code for feet and checked type and valse

--- test/quantityMeasurementTest.js
+++ test/quantityMeasurementTest.js
@@ -1,5 +1,6 @@
 let assert = require('assert');
 let inch = require('../main/inch');
+let feet = require('../main/feet');
 describe('Inch to Inch', function() {
     it('should return true if 0inch and 0inch passed', function() {
         inch1 = new inch(0);
@@ -14,4 +15,34 @@ describe('Inch to Inch', function() {
         let inchObj = inch1.equal(inch1,inch2);
         assert.equal(inchObj,false);
     });
+
+    it('should return false if 1inch and 1.5inch passed', function() {
+        inch1 = new inch(1);
+        inch2 = new inch(1.5);
+        let inchObj = inch1.equal(inch1,inch2);
+        assert.equal(inchObj,false);
+    });
 });
+
+describe('feet to feet', function() {
+    it('should return true if 0feet and 0feet passed', function() {
+        feet1 = new feet(0);
+        feet2 = new feet(0);
+        let feetObj = feet1.equal(feet1,feet2);
+        assert.equal(feetObj,true);
+    });
+
+    it('should return false if null and 1feet passed', function() {
+        feet1 = new feet(null);
+        feet2 = new feet(0);
+        let feetObj = feet1.equal(feet1,feet2);
+        assert.equal(feetObj,false);
+    });
+
+    it('should return false if 1feet and 1.5feet passed', function() {
+        feet1 = new feet(1);
+        feet2 = new feet(1.5);
+        let feetObj = feet1.equal(feet1,feet2);
+        assert.equal(feetObj,false);
+    });
+})
