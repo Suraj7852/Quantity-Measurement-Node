4fc5c7f13708b2164f855cec254ecaa947fa4b98
suraj7852 <surajpj7852@gmail.com>
2019-12-30 17:12:38 +0530

    Added code for checking type and value

---
+++ test/quantityMeasurementTest.js
@@ -0,0 +1,17 @@
+let assert = require('assert');
+let inch = require('../main/quantityMeasurement');
+describe('Inch to Inch', function() {
+    it('should return true if 0inch and 0inch passed', function() {
+        inch1 = new inch(0);
+        inch2 = new inch(0);
+        let inchObj = inch1.equal(inch1,inch2);
+        assert.equal(inchObj,true);
+    });
+
+    it('should return true if 1inch and 1inch passed', function() {
+        inch1 = new inch(null);
+        inch2 = new inch(1);
+        let inchObj = inch1.equal(inch1,inch2);
+        assert.equal(inchObj,false);
+    });
+});
